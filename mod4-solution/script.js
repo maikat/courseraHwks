fc037487879fa29dba27b9b7f71b079817ab3243
ΚΑΡΑΜΠΑΜΠΑ ΜΑΡΙΑ <karababm@MEGATV.COM>
2016-11-11 15:20:42 +0200

    v28

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -48,9 +48,9 @@ for (var i = 0; i < names.length; i++) {
   // Use the string object's 'charAt' function. Since we are looking for
   // names that start with either upper case or lower case 'J'/'j', call
   // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
+  // to lower case character 'j' afterwards. console.log('ALPHABET'.toLowerCase());
   // Look up these methods on Mozilla Developer Network web site if needed.
-  var firstLetter = LowerCase(charAt(names[i],1,1));
+  var firstLetter = charAt(names[i],1,1).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
